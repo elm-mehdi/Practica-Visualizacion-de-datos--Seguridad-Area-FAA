d45adef8742c2d013bff84e21583eb8ff19eaafb
elm-mehdi <musicadeeden@gmail.com>
2026-06-02 23:34:08 +0200

    Update Code.R

--- TOP Nbr of Engines (número de motores de las aeronaves) por eventos/Code.R
+++ TOP Nbr of Engines (número de motores de las aeronaves) por eventos/Code.R
@@ -1,1 +1,27 @@
+data <- read.csv2(
+  "C:/Users/mehdi/Downloads/faa_incidents_data.csv",
+  stringsAsFactors = FALSE,
+  check.names = FALSE
+)
 
+data <- data[, !duplicated(colnames(data)) & colnames(data) != ""]
+
+
+data$`Nbr of Engines`[is.na(data$`Nbr of Engines`) | data$`Nbr of Engines` == ""] <- "DESCONOCIDO"
+
+
+data_filtered <- data[data$`Nbr of Engines` %in% c("1", "2", "3", "4"), ]
+
+
+incidentes_by_nbr_engines <- table(data_filtered$`Nbr of Engines`)
+
+
+print(incidentes_by_nbr_engines)
+
+
+write.csv2(
+  as.data.frame(incidentes_by_nbr_engines),
+  file = "C:/Users/mehdi/Downloads/incidentes_por_nbr_of_engines.csv",
+  row.names = FALSE,
+  quote = TRUE
+)
