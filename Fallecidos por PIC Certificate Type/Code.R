e25c50bb8d8bd86e89b882cca35a505209f04199
elm-mehdi <musicadeeden@gmail.com>
2026-06-03 02:23:27 +0200

    Update Code.R

--- Fallecidos por PIC Certificate Type/Code.R
+++ Fallecidos por PIC Certificate Type/Code.R
@@ -1,1 +1,21 @@
+library(dplyr) 
 
+data <- read.csv2(
+  "C:/Users/mehdi/Downloads/faa_incidents_data.csv",
+  stringsAsFactors = FALSE,
+  check.names = FALSE
+)
+
+data <- data[, !duplicated(colnames(data)) & colnames(data) != ""]
+
+fatalidades_by_certificate <- data %>%
+  group_by(`PIC Certificate Type`) %>%
+  summarise(`Total Fatalities` = sum(`Total Fatalities`, na.rm = TRUE)) %>%
+  arrange(desc(`Total Fatalities`))
+
+write.csv2(
+  fatalidades_by_certificate,
+  file = "C:/Users/mehdi/Downloads/fatalidades_by_certificate.csv",
+  row.names = FALSE,
+  quote = TRUE
+)
